a237ffa4c3bf07053506d6d739ca667d63d0e025
Aleksei Levanovich <levanovich_aa@icloud.com>
2020-06-28 23:06:36 +0300

    H

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/testproject-angular'));
+app.use(express.static(__dirname + '/src/testproject-angular'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/testproject-angular/index.html'));
+app.get('/*', function (req, res) {
+
+  res.sendFile(path.join(__dirname + '/dist/testproject-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
