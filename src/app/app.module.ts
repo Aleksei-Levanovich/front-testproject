b1133e22d9d77225016752622875b1ded732a51d
Aleksei Levanovich <levanovich_aa@icloud.com>
2020-06-30 16:45:20 +0300

    Everything without MATERIAL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import { ProjectsListComponent } from './projects-list/projects-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/projects-list/projects-list.component.ts
+++ src/app/projects-list/projects-list.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import {Project} from "../Project";
 import {plainToClass} from "class-transformer";
 import {Todo} from "../Todo";
+import {FormControl, FormGroup} from "@angular/forms";
 
 
 @Component({
@@ -15,11 +16,17 @@ export class ProjectsListComponent implements OnInit {
   projects : Array<Project>;
   range : number;
   project :  Project;
-  todos : Todo[];
+  current : number;
+
+  addTodo : FormGroup = new FormGroup({
+    "text" : new FormControl(),
+    "name" : new FormControl()
+  })
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.current = -1;
     this.projects=[];
     this.http.get<Project[]>('https://aqueous-scrubland-05247.herokuapp.com/project/')
       .subscribe(data => {
@@ -41,6 +48,65 @@ export class ProjectsListComponent implements OnInit {
     let url = 'https://aqueous-scrubland-05247.herokuapp.com/projects/'+project_id+'/todo/'+id;
     console.log(url);
     this.http.patch(url,{}).subscribe();
+    let todo = this.searchTodo(id, project_id);
+    todo.isCompleted = !todo.isCompleted;
+  }
+
+  public searchTodo(id: number, project_id: number){
+    for (let i=0; i<this.projects.length; i++){
+      if (this.projects[i].id==project_id){
+        for (let j=0; j<this.projects[i].todos.length; j++){
+          if (this.projects[i].todos[j].id==id){
+            return this.projects[i].todos[j];
+          }
+        }
+      }
+    }
+  }
+
+  public popup(int: number){
+    this.current=int;
+  }
+
+  public postTodo(){
+    let text = (document.getElementsByName('text').item(0) as HTMLInputElement).value;
+    let lastTodoId = 0;
+    let lastProjectId = 0;
+    for (let i=0; i<this.projects.length;i++){
+      if (this.projects[i].id>lastProjectId){
+        lastProjectId = this.projects[i].id;
+      }
+      for (let j=0; j<this.projects[i].todos.length;j++){
+        if (this.projects[i].todos[j].id>lastTodoId){
+          lastTodoId=this.projects[i].todos[j].id;
+        }
+      }
+    }
+    lastTodoId++;
+    lastProjectId++;
+    let url = 'https://aqueous-scrubland-05247.herokuapp.com/todo';
+    if (this.current==0){
+      let name = (document.getElementsByName("name").item(0) as HTMLInputElement).value;
+      let newTodo = new Todo(lastTodoId, lastProjectId, text, false);
+      let newProject = new Project(lastProjectId, name,[]);
+      newProject.todos.push(newTodo);
+      this.projects.push(newProject);
+      const body = {
+        id: lastTodoId,
+        project_id: lastProjectId,
+        text: text,
+        isCompleted: false,
+        name: name
+      }
+      this.http.post(url,body).subscribe();
+    } else {
+      let todo = new Todo(lastTodoId, this.current, text, false);
+      this.projects[this.current-1].todos.push(todo);
+      console.log(this.projects);
+      this.http.post(url,todo).subscribe();
+    }
+    this.current=-1;
+    this.addTodo.reset();
   }
 
 
